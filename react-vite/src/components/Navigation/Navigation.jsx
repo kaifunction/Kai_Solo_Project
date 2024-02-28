2b399e7cbfab69b65d16f7e361d1921b92fcb5e0
Kai <fengkai0129@gmail.com>
2024-02-27 21:10:32 -0800

    home page layout

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -5,23 +5,32 @@ import "./Navigation.css";
 function Navigation() {
   return (
     <nav>
-      <NavLink to='/'><img src="/Pinterest-logo.png" alt='Logo' style={{width:"30px"}}/></NavLink>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Explore</NavLink>
-        </li>
-        <li>
-          <NavLink to="/pin-create">Create</NavLink>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <ProfileButton />
-        </li>
-      </ul>
+      <div className="nav-bar">
+        <div className="nav-bar-left">
+          <div className="nav-bar-left-logo">
+            <NavLink to="/">
+              <img src="/Pinterest-logo.png" alt="Logo" style={{ width: "30px" }} />
+            </NavLink>
+          </div>
+          <div className="nav-bar-left-links">
+            <NavLink to="/">Home</NavLink>
+
+            <div
+              onClick={() => window.alert("Feature coming soon")}
+              style={{ cursor: "pointer" }}
+            >
+              Explore
+            </div>
+
+            <NavLink to="/pin-create">Create</NavLink>
+
+          </div>
+        </div>
+        <div className="nav-bar-right">
+            <ProfileButton />
+        </div>
+
+      </div>
     </nav>
   );
 }
