957cbdd0ed9334fb46a23d6d8c814285f01d51bd
Kai <fengkai0129@gmail.com>
2024-03-07 10:15:16 -0800

    change CSS

--- react-vite/src/components/DeletePin/DeletePin.jsx
+++ react-vite/src/components/DeletePin/DeletePin.jsx
@@ -2,14 +2,15 @@ import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { thunkDeletePin } from "../../redux/pin";
 import { useNavigate, useParams } from "react-router-dom";
+import "./DeletePin.css";
 
 const DeletePin = () => {
   const { closeModal } = useModal();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const{ pinId } = useParams();
+  const { pinId } = useParams();
 
-  console.log("pinId FROM DELETE PAGE===>", pinId)
+  console.log("pinId FROM DELETE PAGE===>", pinId);
 
   const deletePin = async (e) => {
     e.preventDefault();
@@ -24,14 +25,18 @@ const DeletePin = () => {
   };
 
   return (
-    <>
-      <form className="DeleteForm">
-        <h1>Confirm Delete</h1>
-        <h3>Are you sure you want to delete this Pin?</h3>
-        <button onClick={deletePin}>Yes (Delete Pin)</button>
-        <button onClick={keepPin}>No (Keep Pin)</button>
-      </form>
-    </>
+    <form className="DeleteForm">
+      <div className="delete-container">
+        <div className="delete-text">
+          <h1>Confirm Delete</h1>
+          <h3>Are you sure you want to delete this Pin?</h3>
+        </div>
+        <div className="delete-button">
+          <button onClick={deletePin}>Yes (Delete Pin)</button>
+          <button onClick={keepPin}>No (Keep Pin)</button>
+        </div>
+      </div>
+    </form>
   );
 };
 
