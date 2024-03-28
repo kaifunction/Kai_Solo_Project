1fa79b508e89a0e9c722fac065e75c0273d1a9e9
Kai <fengkai0129@gmail.com>
2024-03-28 11:18:49 -0700

    create and userProfile page logout page

--- react-vite/src/components/CreatePin/CreatePin.jsx
+++ react-vite/src/components/CreatePin/CreatePin.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { thunkPostPin } from "../../redux/pin";
+import { thunkLogin } from "../../redux/session";
+import { useModal } from "../../context/Modal";
+import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
+import  SignupFormModal  from "../SignupFormModal";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import "./CreatePin.css";
 
 function CreatePin() {
@@ -11,6 +16,11 @@ function CreatePin() {
   const [errors, setErrors] = useState({});
   const [validation, setValidation] = useState({});
   const [disabled, setDisabled] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const { closeModal } = useModal();
   // const [hasSubmitted, setHasSubmitted] = useState(false);
 
   const [pinLink, setPinLink] = useState();
@@ -19,6 +29,63 @@ function CreatePin() {
   const [description, setDescription] = useState("");
   const currentUser = useSelector((state) => state.session?.user)
   // console.log("CURRENTUSER====>",currentUser)
+
+
+
+  const ulRef = useRef();
+
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const closeMenu = () => setShowMenu(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const serverResponse = await dispatch(
+      thunkLogin({
+        email,
+        password,
+      })
+    );
+
+    if (serverResponse) {
+      setErrors(serverResponse);
+    } else {
+      closeModal();
+    }
+  };
+
+  const defaultLogin = async (e) => {
+    e.preventDefault();
+
+    await dispatch(
+      thunkLogin({
+        email: "demo@aa.io",
+        password: "password",
+      })
+    );
+
+    closeModal();
+  };
+
+
 
   function onImageChange(e) {
     if (e.target.files && e.target.files[0]) {
@@ -149,7 +216,77 @@ function CreatePin() {
               </button>
             </div>
           </div>
-    </form>) : <h3 style={{padding: '100px 40px', color:'#ff00bb', fontWeight:'normal', fontSize:'24px'}}>Please Log in or Sign up first...</h3>}
+    </form>) :
+    <div>
+    <h3 style={{padding: '140px 40px 20px 40px',margin:'0px', color:'#ff00bb', fontWeight:'normal', fontSize:'24px', display:'flex', flexDirection:'column', alignItems:'center', backgroundColor:'#ff2f0020'}}>Please Log in or Sign up first...</h3>
+    <div className="login-container">
+      <div className="login-top">
+        <h1>Log In</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="login-bottom">
+          <label>
+            Email:
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="login-input"
+            />
+          </label>
+          {errors.email && (
+            <p style={{ margin: "0", fontSize: "12px", color: "#ff00bb" }}>
+              {errors.email}
+            </p>
+          )}
+          <label>
+            Password:
+            <div className="password-input-container">
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="login-input"
+              />
+              {showPassword ? (
+                <FaEyeSlash onClick={() => setShowPassword(false)} style={{ position: 'relative', left: '-10%' }}/>
+              ) : (
+                <FaEye onClick={() => setShowPassword(true)} style={{ position: 'relative', left: '-10%' }}/>
+              )}
+            </div>
+          </label>
+          {errors.password && (
+            <p style={{ margin: "0", fontSize: "12px", color: "#ff00bb" }}>
+              {errors.password}
+            </p>
+          )}
+          <div className="login-button-container">
+            <button type="submit" className="login-button1">Log In</button>
+
+            {/* <button onClick={() => SignupFormModal()}
+            className="login-button1">Sign Up</button> */}
+            <div onClick={toggleMenu}>
+              <OpenModalMenuItem
+                  itemText="Sign Up"
+                  onItemClick={closeMenu}
+                  modalComponent={<SignupFormModal />}
+                />
+            </div>
+
+            <button onClick={defaultLogin} className="login-button2">Demo User</button>
+          </div>
+        </div>
+      </form>
+      {/* <div style={{backgroundColor:'#ff2f0020', background: 'linear-gradient(to bottom, #ff2f0020, #ffffff)', paddingBottom:'13%'}}> */}
+      <div className="bottom-div">
+
+      </div>
+    </div>
+    </div>
+
+    }
     </div>
   );
 }
--- react-vite/src/components/UserProfile/UserProfile.jsx
+++ react-vite/src/components/UserProfile/UserProfile.jsx
@@ -1,10 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { thunkGetPins } from "../../redux/pin";
+import { thunkLogin } from "../../redux/session";
+import { useModal } from "../../context/Modal";
+import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
+import  SignupFormModal  from "../SignupFormModal";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 const UserProfile = () => {
   const dispatch = useDispatch();
+  const [errors, setErrors] = useState({});
+  const [showMenu, setShowMenu] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const { closeModal } = useModal();
   const user = useSelector((store) => store.session.user);
   const allPins = useSelector((state) => state.pins.pins);
   const [showScrollButton, setShowScrollButton] = useState(false);
@@ -55,6 +66,60 @@ const UserProfile = () => {
     };
   }, []);
 
+
+  const ulRef = useRef();
+
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const closeMenu = () => setShowMenu(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const serverResponse = await dispatch(
+      thunkLogin({
+        email,
+        password,
+      })
+    );
+
+    if (serverResponse) {
+      setErrors(serverResponse);
+    } else {
+      closeModal();
+    }
+  };
+
+  const defaultLogin = async (e) => {
+    e.preventDefault();
+
+    await dispatch(
+      thunkLogin({
+        email: "demo@aa.io",
+        password: "password",
+      })
+    );
+
+    closeModal();
+  };
+
   return (
     <>
       {userId ? (
@@ -82,18 +147,81 @@ const UserProfile = () => {
             </div>
           )}
         </>
-      ) : (
+      ) :
+      <div>
         <h3
-          style={{
-            padding: "100px 40px",
-            color: "#ff00bb",
-            fontWeight: "normal",
-            fontSize: "24px",
-          }}
+          style={{padding: '140px 40px 20px 40px',margin:'0px', color:'#ff00bb', fontWeight:'normal', fontSize:'24px', display:'flex', flexDirection:'column', alignItems:'center', backgroundColor:'#ff2f0020'}}
         >
           Please Log in or Sign up first...
         </h3>
-      )}
+        <div className="login-container">
+      <div className="login-top">
+        <h1>Log In</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="login-bottom">
+          <label>
+            Email:
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="login-input"
+            />
+          </label>
+          {errors.email && (
+            <p style={{ margin: "0", fontSize: "12px", color: "#ff00bb" }}>
+              {errors.email}
+            </p>
+          )}
+          <label>
+            Password:
+            <div className="password-input-container">
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="login-input"
+              />
+              {showPassword ? (
+                <FaEyeSlash onClick={() => setShowPassword(false)} style={{ position: 'relative', left: '-10%' }}/>
+              ) : (
+                <FaEye onClick={() => setShowPassword(true)} style={{ position: 'relative', left: '-10%' }}/>
+              )}
+            </div>
+          </label>
+          {errors.password && (
+            <p style={{ margin: "0", fontSize: "12px", color: "#ff00bb" }}>
+              {errors.password}
+            </p>
+          )}
+          <div className="login-button-container">
+            <button type="submit" className="login-button1">Log In</button>
+
+            {/* <button onClick={() => SignupFormModal()}
+            className="login-button1">Sign Up</button> */}
+            <div onClick={toggleMenu}>
+              <OpenModalMenuItem
+                  itemText="Sign Up"
+                  onItemClick={closeMenu}
+                  modalComponent={<SignupFormModal />}
+                />
+            </div>
+
+            <button onClick={defaultLogin} className="login-button2">Demo User</button>
+          </div>
+        </div>
+      </form>
+      {/* <div style={{backgroundColor:'#ff2f0020', background: 'linear-gradient(to bottom, #ff2f0020, #ffffff)', paddingBottom:'13%'}}> */}
+      <div className="bottom-div">
+
+      </div>
+    </div>
+
+      </div>
+      }
     </>
   );
 };
